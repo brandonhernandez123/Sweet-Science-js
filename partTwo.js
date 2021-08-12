11d214912d43ea983e5bb7631e4d7e712391d709
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-08-12 10:13:17 -0600

    Chapter 2 room one 80% complete

--- partTwo.js
+++ partTwo.js
@@ -7,13 +7,13 @@ const secondButton = document.querySelector('.p2button2')
 const thirdButton = document.querySelector('.p2button3')
 const buttonNext = document.querySelector('.p2next')
 const text = document.querySelector('.text')
-const spaghetti = document.querySelector('.spaghetti')
+const pasta = document.querySelector('.pasta')
 const toolBox = document.querySelector('.toolbox')
 const photo = document.querySelector('.photo')
 const keys = document.querySelector('.keys')
-const bedroom = document.querySelector('.door')
+const bedroom = document.querySelector('.bedroom')
 let gameStart = false
-
+console.log(pasta)
 const gameActive = function () {
   gameStart = true
   text.innerText = `You arrived at the Peterson residence
@@ -23,6 +23,8 @@ const gameActive = function () {
   secondButton.style.visibility = `hidden`
   thirdButton.style.visibility = `hidden`
   buttonNext.style.visibility = `hidden`
+  bedroom.style.visibility = `hidden`
+  keys.style.visibility = `hidden`
 }
 
 const clickDetective = function () {
@@ -38,8 +40,32 @@ function clickKey() {
   keys.addEventListener('click', function () {
     text.innerText = `You found the bedroom key`
     keys.style.visibility = `hidden`
+    bedroom.style.visibility = `visible`
+    bedroom.addEventListener('click', function () {
+      window.location = `./bedroom.html`
+    })
   })
 }
+
+pasta.addEventListener('click', function () {
+  text.innerText = `An uneaten plate of pasta`
+  firstButton.style.visibility = 'visible'
+  firstButton.innerText = `Examine Pasta`
+  secondButton.style.visibility = `visible`
+  secondButton.innerText = `Search Pasta`
+  firstButton.addEventListener('click', function () {
+    firstButton.style.visibility = `hidden`
+    text.innerText = `This pasta has teeth in it that's disgusting!`
+  })
+  secondButton.addEventListener('click', function () {
+    secondButton.style.visibility = `hidden`
+    text.innerText = `You find a key in the pasta
+      
+      David: Someone is playing mind games.`
+    pasta.style.visibility = `hidden`
+    keys.style.visibility = `visible`
+  })
+})
 
 clickKey()
 clickDetective()
