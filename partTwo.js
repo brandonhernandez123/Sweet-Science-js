8672baf406ee3440e4a925e9896422f2c00c06cf
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-08-12 11:51:37 -0600

    chapter 2 part one completed

--- partTwo.js
+++ partTwo.js
@@ -12,8 +12,12 @@ const toolBox = document.querySelector('.toolbox')
 const photo = document.querySelector('.photo')
 const keys = document.querySelector('.keys')
 const bedroom = document.querySelector('.bedroom')
+const bloodyNote = document.querySelector('.bloodynote')
+const pantryLock = document.querySelector('.pantrylock')
+const pantryKey = document.querySelector('.pantrykey')
+
 let gameStart = false
-console.log(pasta)
+
 const gameActive = function () {
   gameStart = true
   text.innerText = `You arrived at the Peterson residence
@@ -25,6 +29,8 @@ const gameActive = function () {
   buttonNext.style.visibility = `hidden`
   bedroom.style.visibility = `hidden`
   keys.style.visibility = `hidden`
+  pantryKey.style.visibility = `hidden`
+  pantryLock.style.visibility = `hidden`
 }
 
 const clickDetective = function () {
@@ -64,6 +70,55 @@ pasta.addEventListener('click', function () {
       David: Someone is playing mind games.`
     pasta.style.visibility = `hidden`
     keys.style.visibility = `visible`
+  })
+})
+
+photo.addEventListener('click', function () {
+  text.innerText = `Its a family portrait but the fathers picture has his face cut out`
+})
+
+toolBox.addEventListener('click', function () {
+  text.innerText = `I wonder why there is a toolbox sitting in the middle of the kitchen...`
+  firstButton.style.visibility = `visible`
+  firstButton.innerText = `Search Toolbox?`
+  firstButton.addEventListener('click', function () {
+    text.innerText = `A screwdriver is missing and there is plyers with blood all over them
+    
+    You found a key in the toolbox`
+    firstButton.style.visibility = `hidden`
+    pantryKey.style.visibility = `visible`
+    toolBox.style.visibility = `hidden`
+  })
+})
+
+bloodyNote.addEventListener('click', function () {
+  text.innerText = `There is a bloody note stuck to the wall...`
+  firstButton.style.visibility = `visible`
+  firstButton.innerText = `Examine Bloody note`
+  secondButton.style.visibility = `visible`
+  secondButton.innerText = `Read Note?`
+  firstButton.addEventListener('click', function () {
+    text.innerText = `The blood is fresh...
+      
+      Reid: Stay sharp David this blood is fresh`
+    firstButton.style.visibility = `hidden`
+  })
+  secondButton.addEventListener('click', function () {
+    text.innerText = `Note reads: This whole family is a waste and whoever you are investigating this... You're making a huge mistake
+    
+    David: Someone had this all planned out.`
+    secondButton.style.visibility = `hidden`
+  })
+})
+
+pantryKey.addEventListener('click', function () {
+  text.innerText = `You found the pantry key`
+  pantryLock.style.visibility = `visible`
+  pantryKey.style.visibility = `hidden`
+  pantryLock.addEventListener('click', function () {
+    text.innerText = `You find a jar with eyeballs
+        
+        Reid: This is brutal, we need to find who did this!`
   })
 })
 
